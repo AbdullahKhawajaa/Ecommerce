00d7f0207cd1737a94219a9fb190fcb388797636
aqsashahzad941 <ameer.nu@gmail.com>
2022-04-21 10:20:50 +0500

    Created Screens

--- routes/index.js
+++ routes/index.js
@@ -7,39 +7,33 @@ router.get("/", forwardAuthenticated, (req, res) =>
   res.render("welcome", { layout: "layouts/layout" })
 );
 
-<<<<<<< HEAD
 // Dashboard
-router.get("/products", ensureAuthenticated, (req, res) =>
-  res.render("products", {
-=======
-
-router.get("/dashboard", ensureAuthenticated, (req, res) =>
-  res.render("dashboard", {
->>>>>>> refs/remotes/origin/main
+router.get("/cart", ensureAuthenticated, (req, res) =>
+  res.render("cart", {
     user: req.user,
     layout: "layouts/layout"
   })
 );
 
-router.get("/adminwelcome", ensureAuthenticated, (req, res) =>
-  res.render("adminwelcome", {
-    user: req.user,
-    layout: "layouts/layout"
-  })
-);
+// router.get("/adminwelcome", ensureAuthenticated, (req, res) =>
+//   res.render("adminwelcome", {
+//     user: req.user,
+//     layout: "layouts/layout"
+//   })
+// );
 
-router.get("/adminaddproduct", ensureAuthenticated, (req, res) =>
-  res.render("adminAddProduct", {
-    user: req.user,
-    layout: "layouts/layout"
-  })
-);
+// router.get("/adminaddproduct", ensureAuthenticated, (req, res) =>
+//   res.render("adminAddProduct", {
+//     user: req.user,
+//     layout: "layouts/layout"
+//   })
+// );
 
-router.get("/adminPayment", ensureAuthenticated, (req, res) =>
-  res.render("adminPayment", {
-    user: req.user,
-    layout: "layouts/layout"
-  })
-);
+// router.get("/adminPayment", ensureAuthenticated, (req, res) =>
+//   res.render("adminPayment", {
+//     user: req.user,
+//     layout: "layouts/layout"
+//   })
+// );
 
 module.exports = router;
